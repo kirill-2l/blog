208a1a36a7032780ac25f57f3d17d05b4d3d2e9d
Petrov Kirill <petrovkirill@gmail.com>
2024-03-03 13:09:11 -0300

    feat: init project

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+import './styles/index.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import AppRouter from "app/providers/ThemeProvider/router/ui/AppRouter";
+
+
+const App = () => {
+   const { theme, toggleTheme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <AppRouter />
+        </div>
+    );
+};
+
+export default App;
