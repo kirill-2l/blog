d9f2b394746e17e3e5b3e7f5274f7a4dcde1dfc5
Petrov Kirill <petrovkirill@gmail.com>
2024-03-04 23:02:04 -0300

    feat: react error boundary

---
+++ src/widgets/PageError/PageError.tsx
@@ -0,0 +1,25 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+    className?: string,
+}
+
+export const PageError = ({ className }: PageErrorProps) => {
+    const { t } = useTranslation();
+    const refreshPage = () => location.reload();
+
+    return (
+        <div className={classNames(cls.PageError)}>
+            <p>{t('Errors sometimes happen')}</p>
+            <Button
+                theme={ThemeButton.CLEAR}
+                onClick={refreshPage}
+            >
+                <p>{t('Refresh page')}</p>
+            </Button>
+        </div>
+    );
+};
