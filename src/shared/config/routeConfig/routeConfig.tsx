4d4526151a3a2724abafa0cf05afdfe6ef70c79f
Petrov Kirill <petrovkirill@gmail.com>
2024-03-04 22:23:37 -0300

    feat: 404 route

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { type RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFound';
 
 export enum AppRoutes {
-  MAIN = 'main',
-  ABOUT = 'about'
+    MAIN = 'main',
+    ABOUT = 'about',
+    NOT_FOUND = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -18,7 +21,11 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
         element: <MainPage />,
     },
     [AppRoutes.ABOUT]: {
-        path: RoutePath.main,
+        path: RoutePath.about,
         element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 };
