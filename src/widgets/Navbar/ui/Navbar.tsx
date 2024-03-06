d30d2f9de95420ed38fad913ae3d8372dcb69f04
Petrov Kirill <petrovkirill@gmail.com>
2024-03-06 15:23:02 -0300

    feat: modal component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinksTheme } from 'shared/ui/AppLink/AppLink';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,8 +10,33 @@ interface NavbarProps {
 }
 
 function Navbar({ className }: NavbarProps) {
+    const [isAuthModalOpened, setIsAuthModalOpened] = useState(false);
+    const { t } = useTranslation();
+
+    const onToggleModal = useCallback(
+        () => setIsAuthModalOpened((prev) => !prev),
+        [setIsAuthModalOpened],
+    );
     return (
-        <div className={classNames(cls.navbar)} />
+
+        <div className={classNames(cls.navbar)}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                onClick={onToggleModal}
+            >
+                {t('Log in')}
+            </Button>
+            <Modal
+                isOpen={isAuthModalOpened}
+                onClose={onToggleModal}
+            >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Autem perspiciatis, sint. Beatae fuga iure
+                natus sunt suscipit ut? Doloribus, eveniet.
+
+            </Modal>
+
+        </div>
     );
 }
 
