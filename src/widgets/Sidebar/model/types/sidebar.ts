5783875da7d5f51600e995fcec7a80be69267653
Petrov Kirill <petrovkirill@gmail.com>
2024-04-07 15:51:01 -0300

    feat: sidebar user profile link

--- src/widgets/Sidebar/model/types/sidebar.ts
+++ src/widgets/Sidebar/model/types/sidebar.ts
@@ -2,15 +2,14 @@ import React from 'react';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
-import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
 import ArticleIcon from 'shared/assets/icons/article-20-20.svg';
+import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
 
 export type SidebarItemType = {
     path: string;
     text: string
     Icon: React.VFC<React.SVGProps<SVGSVGElement>>
 }
-
 export const SidebarItemList: SidebarItemType[] = [
     {
         path: RoutePath.main,
