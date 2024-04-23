4b30b8d22409366f3ad729b2209dc4ad08ceb3ce
Petrov Kirill <petrovvkirill@gmail.com>
2024-04-23 11:54:07 -0300

    refactor: handle ts code with babel, custom babel plugin for removing test attributes

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -1,15 +1,33 @@
 import { BuildOptions } from '../types/config';
+import BabelRemovePropsPlugin from '../../babel/babelRemoveProps.plugin';
 
-export const buildBabelLoader = (options: BuildOptions) => {
-    const { isDev } = options;
+interface buildBabelLoaderOptions extends BuildOptions {
+    isTsx: boolean;
+}
+
+export const buildBabelLoader = (options: buildBabelLoaderOptions) => {
+    const {
+        isDev,
+        isTsx,
+    } = options;
     return {
-        test: /\.(js|ts|tsx)$/,
+        test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
         exclude: /node_modules/,
         use: {
             loader: 'babel-loader',
             options: {
                 presets: ['@babel/preset-env'],
-                plugins: [isDev && require.resolve('react-refresh/babel')].filter(Boolean),
+                plugins: [
+                    ['@babel/plugin-transform-typescript', {
+                        isTsx,
+                    }],
+                    '@babel/plugin-transform-runtime',
+                    isTsx && [BabelRemovePropsPlugin, {
+                        props: ['data-testid'],
+                    }],
+                    isDev && require.resolve('react-refresh/babel'),
+
+                ].filter(Boolean),
             },
         },
     };
